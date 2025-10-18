e04abb24bf8d3a3e1da09e3a2274fc7b2d20eab8
Huihua Huang <addiehuanghuihua@gmail.com>
2025-10-18 15:38:36 -0400

    build and register models for menu app

--- backend/menu/admin.py
+++ backend/menu/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Allergen, MenuCategory, MenuItem, ItemAllergen
 # Register your models here.
+admin.site.register(Allergen)
+admin.site.register(MenuCategory)
+admin.site.register(MenuItem)
+admin.site.register(ItemAllergen)
